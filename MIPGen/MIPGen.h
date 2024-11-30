f852651a7854548cf1e97e503dbae61075ea8475
StarsX <X-Universe@outlook.com>
2024-11-30 10:17:11 +0800

    Fix auto adapter selection and refine timer

--- MIPGen/MIPGen.h
+++ MIPGen/MIPGen.h
@@ -39,6 +39,13 @@ public:
 	virtual void ParseCommandLineArgs(wchar_t* argv[], int argc);
 
 private:
+	enum DeviceType : uint8_t
+	{
+		DEVICE_DISCRETE,
+		DEVICE_UMA,
+		DEVICE_WARP
+	};
+
 	static const uint8_t FrameCount = 3;
 
 	XUSG::DescriptorTableLib::sptr	m_descriptorTableLib;
@@ -65,10 +72,11 @@ private:
 	uint64_t	m_fenceValues[FrameCount];
 
 	// Application state
+	DeviceType	m_deviceType;
+	StepTimer	m_timer;
 	MipGenerator::PipelineType m_pipelineType;
 	bool		m_showFPS;
 	bool		m_isPaused;
-	StepTimer	m_timer;
 
 	// User external settings
 	std::wstring m_fileName;
